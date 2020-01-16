785a1798849e3ce6ecf7e4cb4146d104821818f4
RobBlanchard <robin.blanchard00@gmail.com>
2020-01-16 10:44:23 +0100

    Generate new array button + refacto

--- sorting-visualizer/src/App.js
+++ sorting-visualizer/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 
 function App() {
--- sorting-visualizer/src/SortingVisualizer/SortingVisualizer.jsx
+++ sorting-visualizer/src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Rectangle from './Rectangle.jsx'
+import Button from 'react-bootstrap/Button';
+
 import './SortingVisualizer.css'
+
 
 
 const ARRAY_LENGTH=10
@@ -11,24 +14,32 @@ export default class SortingVisualizer extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            array : this.generate_new_array(ARRAY_LENGTH, ARRAY_MIN, ARRAY_MAX)
+            array : []
         }
       }
-
-    render(){
-        return (<div className="container" style={{ background: '#9e9e9e', width: '80vw', height: '80vh' }}>
-            {this.state.array.map(number =>
-                (<Rectangle color={'turquoise'} height={Math.round(100*number/ARRAY_MAX)} 
-                            width={Math.round(100/ARRAY_LENGTH)} value={ARRAY_LENGTH<=10 ? number : ""}></Rectangle>)
-            )}
-      </div>)
+    
+    componentDidMount() {
+        this.generate_new_array(ARRAY_LENGTH, ARRAY_MIN, ARRAY_MAX);
     }
 
+    render(){
+        return (<div>
+                <div className="arrayContainer" style={{ background: '#9e9e9e', width: '80vw', height: '80vh' }}>
+                    {this.state.array.map((number,idx) =>
+                            (<Rectangle key={idx} color={'turquoise'} height={Math.round(100*number/ARRAY_MAX)} 
+                            width={Math.round(100/ARRAY_LENGTH)} value={ARRAY_LENGTH<=10 ? number : ""}></Rectangle>)
+                    )}
+                </div>
+                <Button onClick={() => this.generate_new_array(ARRAY_LENGTH, ARRAY_MIN, ARRAY_MAX)}>Generate new array</Button>
+                </div>
+            )
+    }
+    
     generate_new_array(len, min, max) {
         var array = []
         for (var i = 0; i<len; i++){
-            array.push(Math.round(min + (max-min)*Math.random()))
+            array.push(Math.round(ARRAY_MIN + (ARRAY_MAX-ARRAY_MIN)*Math.random()))
         }
-        return array
+        this.setState({array: array});
     }
 }
