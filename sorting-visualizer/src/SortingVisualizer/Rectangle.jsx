5cc466f4acf85411cd60333d78a4166fd850514a
RobBlanchard <robin.blanchard00@gmail.com>
2020-01-20 09:28:39 +0100

    Rectangles moved upside down

--- sorting-visualizer/src/SortingVisualizer/Rectangle.jsx
+++ sorting-visualizer/src/SortingVisualizer/Rectangle.jsx
@@ -3,14 +3,28 @@ import React from 'react';
 
 export default class Rectangle extends React.Component{
     render() {
-        var style = {backgroundColor : this.props.color,
+        var styleChild = {
+            position : "absolute",
+            backgroundColor : this.props.color,
+            width : "100%",
+            height : this.props.height.toString()+"%",
+            top : (100-this.props.height).toString()+"%",
+            };
+
+        var styleParent = {
+            position: "relative",
             width : this.props.width.toString()+"%",
-            height : this.props.height.toString()+"%"}
+            height : "100%"
+            
+        };
 
         return (
-            <div className="rectangle" style={style}>
-                {this.props.value}
+            <div className="rectangle" style={styleParent}>
+                <div style={styleChild}>
+                    {this.props.value}
+                </div>
             </div>
+            
         )
     }
 }
